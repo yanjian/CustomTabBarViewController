1a0392117fc9f565b6a3b14df9e56f6dfbd591ab
yanjaya <yanjian@YanJians-MacBook-Pro.local>
2017-04-12 20:38:58 +0800

    调整tableview的间距

--- Weiboemo/Weiboemo/Classes/View(视图和控制器)/Home/WBHomeViewController.swift
+++ Weiboemo/Weiboemo/Classes/View(视图和控制器)/Home/WBHomeViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
+private let cellid = "HOMECELLID"
+
 class WBHomeViewController: WBBaseViewController {
 
+    lazy var listData:[String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,15 +31,40 @@ class WBHomeViewController: WBBaseViewController {
         navigationController?.pushViewController(vc, animated: true)
         
     }
+    
+    override func loadData() {
+        for t in 1..<18 {
+            listData.append(String(t))
+        }
+    }
 
 }
 
+extension WBHomeViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return listData.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+       let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath)
+        cell.textLabel?.text = listData[indexPath.row]
+        
+        return cell
+       
+    }
+
+
+}
 
 extension WBHomeViewController{
     override func setupUI() {
         super.setupUI()
         
         navItem.leftBarButtonItem = UIBarButtonItem(title: "好友", tagter: self, action: #selector(showFriend))
+       
+        homeTableview?.register(UITableViewCell.self, forCellReuseIdentifier: cellid)
         
     }
 }
--- Weiboemo/Weiboemo/Classes/View(视图和控制器)/Main/WBBaseViewController.swift
+++ Weiboemo/Weiboemo/Classes/View(视图和控制器)/Main/WBBaseViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class WBBaseViewController: UIViewController {
-
+    
+    public var homeTableview :UITableView?
+    
+    
     public lazy var navigationBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 64))
     
     public lazy var navItem = UINavigationItem()
@@ -18,12 +21,18 @@ class WBBaseViewController: UIViewController {
         super.viewDidLoad()
 
         setupUI()
+        automaticallyAdjustsScrollViewInsets = false
+        loadData()
     }
     
     override var title: String?{
         didSet{
             navItem.title = title 
         }
+    }
+    
+    public func loadData(){
+        
     }
 
 }
@@ -43,5 +52,34 @@ extension WBBaseViewController{
         navigationBar.items = [navItem]
         
         navigationBar.barTintColor = UIColor.white
+        
+        setupTableViewUI()
+    }
+    
+    public func setupTableViewUI() {
+        homeTableview = UITableView(frame: view.bounds, style: .plain)
+        
+        view.insertSubview(homeTableview!, belowSubview: navigationBar)
+        
+        homeTableview?.delegate = self
+        homeTableview?.dataSource = self
+        
+        homeTableview?.contentInset = UIEdgeInsets(top: navigationBar.bounds.height, left: 0, bottom: tabBarController?.tabBar.bounds.height ?? 0, right: 0)
+        
     }
 }
+
+
+extension WBBaseViewController:UITableViewDelegate,UITableViewDataSource{
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 0
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        return UITableViewCell()
+    }
+    
+}
+
+
